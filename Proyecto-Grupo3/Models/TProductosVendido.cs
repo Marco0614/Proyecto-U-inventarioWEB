bf5ddcfc7734e4dd6742ebde5fd20d1fa9702d17
Ale-Feng <alefeng297@gmail.com>
2023-12-10 21:00:04 -0600

    Estilos de la farmacia

--- Proyecto-Grupo3/Models/TProductosVendido.cs
+++ Proyecto-Grupo3/Models/TProductosVendido.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Proyecto_Grupo3.Models
 {
@@ -10,11 +11,28 @@ namespace Proyecto_Grupo3.Models
             TFacturas = new HashSet<TFactura>();
         }
 
+        [Required]
+        [Display(Name = "Codigo Producto")]
         public int CodigoProducto { get; set; }
+
+        [Required]
+        [Display(Name = "Codigo Tipo Producto")]
         public int CodigoTipoProducto { get; set; }
+
+        [Required]
+        [Display(Name = "Descripcion Producto")]
         public string DescripcionProducto { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "Precio")]
         public decimal Precio { get; set; }
+
+        [Required]
+        [Display(Name = "Estado")]
         public string Estado { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "Cantidad")]
         public int Cantidad { get; set; }
 
         public virtual TTiposProducto CodigoTipoProductoNavigation { get; set; } = null!;
