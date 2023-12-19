8b1870fd223f68940091287b10b6d11a7a73e5a8
bryma <bryma@MCQ>
2023-12-19 00:10:08 -0600

    Validaciones de Roles

--- Proyecto-Grupo3/Controllers/FacturasController.cs
+++ Proyecto-Grupo3/Controllers/FacturasController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,7 @@ using Proyecto_Grupo3.Models;
 
 namespace Proyecto_Grupo3.Controllers
 {
+    [Authorize(Roles = "Administrador,Vendedor,Contador")]
     public class FacturasController : Controller
     {
         private readonly DB_FARMACIAContext _context;
